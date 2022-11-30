e7861cced4279c664e0b1c09c65ad8740302e663
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-30 13:29:13 -0500

    First

---
+++ atterraggio.php
@@ -0,0 +1,24 @@
+<?php
+
+$paragrafo = $_GET['paragrafo'];
+$parolaDaCensurare = $_GET['parola da censurare']
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Atterraggio</title>
+</head>
+<body>
+  <h1> 
+    Ciao <?php $paragrafo ?>  <?php $parolaDaCensurare ?>
+
+  </h1>
+  
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+
+
+<form action="./atterraggio.php" method="GET">
+    <input type="text" name="paragrafo" placeholder="paragrafo" /><br>
+    <input type="text" name="parola da censurare" placeholder="parola da censurare"/><br>
+    <button type="submit">Invia</button>
+</form>
+
+</body>
+</html>
+
